264b686a480c103daaa7994429966c24b4f5cbd3
phillt3 <tracypb@bc.edu>
2020-09-05 17:11:13 -0400

    Prettying up, adding comments, more to do

--- processes/processA.go
+++ processes/processA.go
@@ -16,9 +16,9 @@ func main() {
 	-Does this by checking if port was provided and proceeds to attempt to connect with that port (net.Dial)
 	^^^This is portion is mainly implemented from Linode.com exercise "Create a TCP and UDP Client and Server using Go"
 	-loop does most of the work, reading in specific information one at a time and writing/sending it to the server
-	-info includes (in order): Title of message, recipient (To), sender (From), date (Date), message (Content)
+	-info includes (in order): recipient (To), Title of message, sender (From), date (Date), message (Content)
 	*/
-	arguments := os.Args //For collecting command-line input
+	arguments := os.Args //For collecting command-line run input (IP and desired port number)
 	if len(arguments) == 1 {
 		//checks case where host port not provided at end of run statement
 		fmt.Println("Please provide host:port.")
@@ -29,7 +29,7 @@ func main() {
 	c, err := net.Dial("tcp", CONNECT)
 	//connection made using given host port to server
 	if err != nil {
-		//if error is returned by net.Dial, is printed here
+		//if error is returned by net.Dial,it is printed
 		fmt.Println(err)
 		return
 	}
@@ -37,15 +37,15 @@ func main() {
 	for {
 		reader := bufio.NewReader(os.Stdin) //reads in input from Stdin (terminal)
 
-		fmt.Print(">>Title: ")
-		text, _ := reader.ReadString('\n')
-		fmt.Fprintf(c, text+"\n")
-		//gathering and sending Title information from user input
-
 		fmt.Print(">>To: ")
 		text2, _ := reader.ReadString('\n')
 		fmt.Fprintf(c, text2+"\n")
 		//gathering and sending recipient name/email address from user input
+
+		fmt.Print(">>Title: ")
+		text, _ := reader.ReadString('\n')
+		fmt.Fprintf(c, text+"\n")
+		//gathering and sending Title information from user input
 
 		fmt.Print(">>From: ")
 		text3, _ := reader.ReadString('\n')
--- processes/processB.go
+++ processes/processB.go
@@ -18,68 +18,76 @@ func main() {
 	^^^This is portion is mainly implemented from Linode.com exercise "Create a TCP and UDP Client and Server using Go"
 	-loop does most of the work, reading in data in specific order, using data to build instance of struct message
 	-parts of struct message are then printed separately but just teh same can be printed all together
-	-info includes (in order): Title of message, recipient (To), sender (From), date (Date), message (Content)
+	-info includes (in order): recipient (To), Title , sender (From), date (Date), message (Content)
 	*/
 
-	arguments := os.Args
+	arguments := os.Args //for collecting command line run inputs (port number)
 	if len(arguments) == 1 {
+		//checking if port number was provided
 		fmt.Println("Please provide port number")
 		return
 	}
 
 	PORT := ":" + arguments[1]
 	l, err := net.Listen("tcp", PORT)
+	//opening channel under given port number, making processB the server
 	if err != nil {
+		//if error is returned by net.Listen, it is printed
 		fmt.Println(err)
 		return
 	}
 	defer l.Close()
+	//the closing statement for the program, will not be executed until all other operations are complete
 
 	c, err := l.Accept()
+	//only after a successful call to Accept() that the TCP server can begin to interact with TCP client (linode.com)
 	if err != nil {
+		//if error is returned by l.Accept, it is printed
 		fmt.Println(err)
 		return
 	}
 
-	var messrec message.Message
+	var messrec message.Message //initializing message struct to be constructed by input sent by client
 
 	for x := 0; x < 5; x++ {
-		netData, err := bufio.NewReader(c).ReadString('\n')
-
+		netData, err := bufio.NewReader(c).ReadString('\n') //reading in input sent to the server
 		if err != nil {
+			//if err is given an error value, it is printed here
 			fmt.Println(err)
 			return
 		}
 
+		//based on iteration of the loop, order is determined for how data sent to the server is interpreted
+		//order is: Recipient then title then sender then date, then message
 		if x == 0 {
-			messrec.Title = string(netData)
-			//fmt.Print(messrec.Title)
+			messrec.To = string(netData) //storing recipient of message in appropriate struct field
 		} else if x == 1 {
-			messrec.To = string(netData)
-			//fmt.Print(messrec.To)
+			messrec.Title = string(netData) //storing Title of message in appropriate struct field
 		} else if x == 2 {
-			messrec.From = string(netData)
-			//fmt.Print(messrec.From)
+			messrec.From = string(netData) //storing sender of message in appropriate struct field
 		} else if x == 3 {
-			messrec.Date = string(netData)
-			//fmt.Print(messrec.Date)
+			messrec.Date = string(netData) //storing date and time of message in appropriate struct field
 		} else if x == 4 {
-			messrec.Content = string(netData)
-			//fmt.Print(messrec.Content)
+			messrec.Content = string(netData) //storing content of message in appropriate struct field
 		}
 
 		t := time.Now()
 		myTime := "Message received at " + t.Format(time.RFC3339) + "\n"
 		c.Write([]byte(myTime))
-		c.Write([]byte("!"))
+		//Above is the explicit ACK that will be seen by the client and user in the terminal
+		//It will provide the time and date the date was received and completely constructed
 
 		if x == 4 {
-			fmt.Print("Title: " + messrec.Title)
+			//final step is to take constructed message and print it to user on the server end
+			//The message has been printed in both a neat formatted version
+			//It has also can be printed in its original struct format
+			//return statement allows previous close() call to be executed
 			fmt.Print("To: " + messrec.To)
+			fmt.Print("Title: " + messrec.Title)
 			fmt.Print("From: " + messrec.From)
 			fmt.Print("Date: " + messrec.Date)
 			fmt.Print("Content: " + messrec.Content)
-			fmt.Printf("Here is message printed in struct format:\n%v\n", messrec)
+			//fmt.Printf("Here is message printed in struct format:\n%v\n", messrec)
 			fmt.Println("Exiting ProcessB TCP server!")
 			return
 		}
